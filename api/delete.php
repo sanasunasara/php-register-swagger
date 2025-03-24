50c6d71fa8fac426d0da3681e95a080effe6c3a3
sanafatem <fsana7428@gmail.com>
2025-03-24 15:04:33 +0530

    commit

--- api/delete.php
+++ api/delete.php
@@ -2,7 +2,6 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 header("Access-Control-Allow-Methods: DELETE");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 include_once '../config/database.php';
 
